a6fcd204620356e7176d8399000df75e81981370
Anthony <anthony.mazzatenta@atsgroup.com>
2019-01-05 19:27:53 -0500

    This is a simple implementation of the gopsutil disk module

---
+++ pkg/collectors/disk-linix.go
@@ -0,0 +1,23 @@
+// +build linux
+
+package collectors
+
+import (
+	"github.com/atssteve/perf_collector/pkg/metrics"
+	"github.com/shirou/gopsutil/disk"
+	log "github.com/sirupsen/logrus"
+)
+
+// GetDiskInfo collects all of the available stats for disk
+func GetDiskInfo(ch chan metrics.Metric) {
+	filesystems, _ := disk.Partitions(true)
+	for _, fsStats := range filesystems {
+		v, _ := disk.Usage(fsStats.Mountpoint)
+		log.WithFields(log.Fields{
+			"collector": "disk",
+			"os":        "linux",
+		}).Info(v)
+
+		ch <- v
+	}
+}
