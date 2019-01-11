c962c6783d4bb5756bd3759fc155734993981005
Anthony <anthony.mazzatenta@atsgroup.com>
2019-01-10 19:56:45 -0500

    Trying something

--- pkg/collectors/disk-darwin.go
+++ pkg/collectors/disk-darwin.go
@@ -3,21 +3,42 @@
 package collectors
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/atssteve/perf_collector/pkg/metrics"
 	"github.com/shirou/gopsutil/disk"
 	log "github.com/sirupsen/logrus"
 )
 
+//LoadDiskData Im al litte off the walls here
+type LoadDiskData struct {
+	DeviceName string
+	FSInfo     disk.UsageStat
+}
+
+func (d LoadDiskData) String() string {
+	// myret := make(map[string]disk.UsageStat)
+	s, _ := json.Marshal(d)
+	return string(s)
+}
+
 // GetDiskInfo collects all of the available stats for disk
 func GetDiskInfo(ch chan metrics.Metric) {
-	filesystems, _ := disk.Partitions(true)
+	filesystems, _ := disk.Partitions(false)
 	for _, fsStats := range filesystems {
+		myNewGuy := LoadDiskData{}
+		myNewGuy.DeviceName = fsStats.Device
+		fmt.Println("HELLO", fsStats)
 		v, _ := disk.Usage(fsStats.Mountpoint)
+		myNewGuy.FSInfo = *v
 		log.WithFields(log.Fields{
 			"collector": "disk",
 			"os":        "darwin",
-		}).Info(v)
-
-		ch <- v
+		}).Info(myNewGuy)
+		// customData := make(map[string]disk.UsageStat)
+		// customData[fsStats.Device] = *v
+		ch <- myNewGuy
+		// ch <- v
 	}
 }
